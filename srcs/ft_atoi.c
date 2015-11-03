d8e1243cf59958d32c9ed1ba7f7b82f8fac58745
timon <timonprata@gmail.com>
2015-11-03 06:34:39 +0100

    le vilain backslash f

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -11,7 +11,7 @@ int	ft_atoi(const char *str)
   i = 0;
   while (str[i] == ' ' || str[i] == '\t' ||
 	 str[i] == '\v' || str[i] == '\r' ||
-	 str[i] == '\n')
+	 str[i] == '\n' || str[i] == '\f')
     i++;
   if (str[i] == '-' || str[i] == '+')
     {
