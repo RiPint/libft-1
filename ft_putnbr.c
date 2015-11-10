d79148537219bc5d64693947f208455d7199ca3d
timon <timonprata@gmail.com>
2015-11-10 19:45:55 +0100

    update

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -2,14 +2,16 @@
 
 void	ft_putnbr(int n)
 {
-  if (n < 0)
+  if (n == -2147483648)
+    ft_putstr("-2147483648");
+  if (n < 0 && n > -2147483648)
     {
       ft_putchar('-');
       n = -n;
     }
-  if (n < 10)
+  if (n < 10 && n > -2147483648)
     ft_putchar(n + 48);
-  if (n > 9)
+  else if (n > 9)
     {
       ft_putnbr(n / 10);
       ft_putnbr(n % 10);
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,16 +2,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-  if (n < 0)
+  if (n == -2147483648)
+    ft_putstr_fd("-2147483648", fd);
+   if (n < 0 && n > -2147483648)
     {
       ft_putchar_fd('-', fd);
       n = -n;
     }
-  if (n < 10)
+   if (n < 10 && n > -2147483648)
     ft_putchar_fd(n + 48, fd);
-  if (n > 9)
+   else if (n > 9)
     {
-      ft_putnbr(n / 10);
-      ft_putnbr(n % 10);
+      ft_putnbr_fd(n / 10, fd);
+      ft_putnbr_fd(n % 10, fd);
     }
 }
