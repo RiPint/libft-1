de4dbf9262bec44188e1b09806b91d96f85c3155
Timon PRATA <tiprata@e3r2p21.42.fr>
2015-11-23 20:27:23 +0100

    lastcommit

--- ft_memchr.c
+++ ft_memchr.c
@@ -15,20 +15,14 @@
 void    *ft_memchr(const void *s, int c, size_t n)
 {
   size_t i;
-
+  
   i = 0;
-  if (c > 127 || c < 0)
-    return (NULL);
-  if (*(unsigned char *) s == (unsigned char) c)
-    return ((void *)s);
-  while (*(unsigned char *) s != (unsigned char) c && *(unsigned char *)s && n--)
-    s++;
-  if (*(unsigned char *) s == (unsigned char) c)
-    return ((void *)s);
-  if (*(unsigned char *) s == '\0')
-    return (NULL);
-  else if (n == 0)
-    return ((void *)s);
-  else
-    return (NULL);
+  while (n--)
+    {
+      if (*(unsigned char *)s == (unsigned char) c)
+	return ((void *)s);
+      if (*(unsigned char *)s != (unsigned char) c)
+	s++;
+    }
+  return (NULL);
 }
