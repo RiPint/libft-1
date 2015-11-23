14537caa8c3d161fbd5cd3d4290a4d1824117651
Timon PRATA <tiprata@e3r2p21.42.fr>
2015-11-23 19:50:46 +0100

    headers

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,16 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/23 19:37:16 by tiprata           #+#    #+#             */
+/*   Updated: 2015/11/23 19:38:26 by tiprata          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void    *ft_memchr(const void *s, int c, size_t n)
 {
   size_t i;
 
   i = 0;
+  if (c > 127 || c < 0)
+    return (NULL);
+  if (*(unsigned char *) s == (unsigned char) c)
+    return ((void *)s);
   while (*(unsigned char *) s != (unsigned char) c && *(unsigned char *)s && n--)
     s++;
+  if (*(unsigned char *) s == (unsigned char) c)
+    return ((void *)s);
   if (*(unsigned char *) s == '\0')
-    return (NULL); 
+    return (NULL);
   else if (n == 0)
     return ((void *)s);
-  else 
+  else
     return (NULL);
 }
