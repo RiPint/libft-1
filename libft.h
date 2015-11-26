103e0eb9b9cd63e35f80ed0489d3ef1ea5ecb9b4
timon <timonprata@gmail.com>
2015-11-27 00:21:25 +0100

    lost .h

--- libft.h
+++ libft.h
@@ -21,7 +21,6 @@ typedef	struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-t_list				*ft_lstnew(void const *content, size_t content_size);
 int					ft_atoi(const char *str);
 int					ft_isprint(int c);
 void				ft_memdel(void **ap);
@@ -79,5 +78,11 @@ char				*ft_strxdup(char const *s, size_t x, size_t y);
 size_t				ft_line_count(char const *s, char c);
 char				**ft_strsplit(char const *s, char c);
 int					ft_toupper(int c);
+void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+void				ft_lstadd(t_list **alst, t_list *new);
+t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list				*ft_lstnew(void const *content, size_t content_size);
 
 #endif
